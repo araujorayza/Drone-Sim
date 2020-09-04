20fb5fdcc140edf1400eebc39f5c4c6090233086
RAB <47280806+araujorayza@users.noreply.github.com>
2020-09-04 10:30:41 -0300

    Fuzzy model 1 is tracking in openloop with errors of order 10^-12

--- Check_FuzzyTracking.m
+++ Check_FuzzyTracking.m
@@ -1,18 +1,23 @@
 %% SCRIPT FOR TESTING THE TRACKING OF TSFUZZY MODEL
 
 %% Check Nonzero entries of the simulation results
-ERRx = ERROR(:,PosX)+ DES_STATE(:,PosX) ...
-     - STATE(:,PosX);
- if(CheckError(ERRx,tol))
-    fprintf('\n-->X NOT TRACKING\n\tErrors between %.2e and %.2e', min(ERRx), max(ERRx))
- else
-	fprintf('\n-->X ok')
- end 
 
+ 
+for i = VelX:PosYaw
+    ERR = ERROR(:,i)+ DES_STATE(:,i) ...
+        - STATE(:,i);
+    if(CheckError(ERR,tol))
+        fprintf('\n-->%dth COMPONENT NOT TRACKING\n\tErrors between %.2e and %.2e',i,min(ERR),max(ERR))
+    else
+        fprintf('\n-->%dth component ok',i)
+    end
+end
+ 
+fprintf('\n')
 %% Function
 function err = CheckError(ERR,tol)
     if(find(ERR)) %if it finds nonzero error entries
-        if(ERR == round(ERR,tol)) 
+        if(ERR == round(ERR,tol))
             err = true; %if it has been rounded, then
                         % the errors are real
         else
